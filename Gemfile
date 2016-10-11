3479d40279b191f6a82f8842c416c9e0a7a122b7
haupp246 <haupham24695@gmail.com>
2016-10-11 11:30:18 -0400

    week5

--- Gemfile
+++ Gemfile
@@ -24,6 +24,9 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass', '3.2.0.0'
 gem 'bcrypt' ,'3.1.7'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
+gem 'faker'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,33 @@
 class SessionsController < ApplicationController
-	  def new
-	  end
-	  def current_user
+	def new
+	end
+	def current_user
 			@current_user ||= User.find_by(id: session[:user_id])
-	  end
-	  def create
+	end
+	def create
 	  	user = User.find_by(email: params[:session][:email].downcase)
 	  	if user && user.authenticate(params[:session][:password])
 	  		log_in user
-	  		redirect_to user
+	  		params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+	  		redirect_back_or user
 	  	else
 	  		flash.now[:danger] = 'Invalid email/password combination'
 	  		render 'new'
 	  	end
 	  end
-	  def log_out
+ 	def forget(user)
+		user.forget
+		cookies.delete(:user_id)
+		cookies.delete(:remember_token)
+	end
+	
+	def log_out
+		forget(current_user)
 	  	session.delete(:user_id)
 	  	@current_user=nil
-	  end
-	  def destroy
-	  	log_out
+	end
+	def destroy
+	  	log_out if logged_in?
 	  	redirect_to root_url
 	  end
 	
